81fe710695e74f7712e78b21b0d41910ab4ed56f
dnyvik <d.nyvik@gmail.com>
2019-06-17 23:12:24 +0200

    Add serializer for EventHandlerState

---
+++ src/Crumbs.Core/Event/EventualConsistency/IEventHandlerStateSerializer.cs
@@ -0,0 +1,8 @@
+﻿namespace Crumbs.Core.Event.EventualConsistency
+{
+    public interface IEventHandlerStateSerializer
+    {
+        string Serialize(IEventHandlerState eventHandlerState);
+        T Deserialize<T>(string data) where T : IEventHandlerState;
+    }
+}
---
+++ src/Crumbs.Serializers.Json/JsonEventHandlerStateSerializer.cs
@@ -0,0 +1,18 @@
+﻿using Crumbs.Core.Event.EventualConsistency;
+using Newtonsoft.Json;
+
+namespace Crumbs.Serializers.Json
+{
+    public class JsonEventHandlerStateSerializer : IEventHandlerStateSerializer
+    {
+        public T Deserialize<T>(string data) where T : IEventHandlerState
+        {
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+
+        public string Serialize(IEventHandlerState eventHandlerState)
+        {
+            return JsonConvert.SerializeObject(eventHandlerState);
+        }
+    }
+}
